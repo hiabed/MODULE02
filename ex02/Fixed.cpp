18b445d8e162667fd8cfc619d39a1ac30966101e
Mohammed-Abed Hassani-Idrissi <mhassani@c3r10p4.1337.ma>
2023-11-13 15:58:28 +0100

    module02 in progress

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,71 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() //default constructor;
+{
+    std::cout << "Default constructor called\n";
+    fixed_point = 0;
+}
+
+Fixed::Fixed(const int nb) //constructor parametrized with int;
+{
+    std::cout << "Int constructor called\n";
+    fixed_point = nb << frac_bits;
+    // (int) converts it to the corresponding fixed-point value.
+}
+
+Fixed::Fixed(const float nb) //constructor parametrized with float;
+{
+    std::cout << "Float constructor called\n";
+    fixed_point = (roundf(nb * (1 << frac_bits)));
+    // (float) converts it to the corresponding fixed-point value.
+}
+
+Fixed::Fixed(const Fixed &copy_constructor) // copy constructor;
+{
+    std::cout << "Copy constructor called\n";
+    *this = copy_constructor;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called\n";
+    return fixed_point;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    fixed_point = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+    return ((float)fixed_point / (1 << frac_bits));
+    // converts the fixed-point value to a floating-point value.
+}
+
+int Fixed::toInt( void ) const
+{
+    int to_int = fixed_point >> frac_bits;
+    return to_int;
+}
+
+Fixed& Fixed::operator=(const Fixed &other) //copy assignement;
+{
+    std::cout << "Copy assignment operator called\n";
+    if (this != &other) // Self-assignment check
+    {
+        fixed_point = other.fixed_point;
+    }
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &output, Fixed const &i)
+{
+    output << i.toFloat();
+    return output;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+#include <cmath>
+
+class Fixed
+{
+  private:
+	int fixed_point;
+	static const int frac_bits = 8;
+
+  public:
+	Fixed();                              // Default constructor
+	Fixed(const Fixed &copy_constructor); // Copy constructor
+	Fixed &operator=(const Fixed &other); // Copy assignment operator
+	Fixed(const int nb);	  			  // Parametrized constructor
+	Fixed(const float nb);				  // Parametrized constructor
+	~Fixed();                             // Destructor
+	void setRawBits(int const raw);
+	int getRawBits(void) const;
+	float toFloat( void ) const;
+	int toInt( void ) const;
+};
+
+std::ostream & operator<<( std::ostream & o, Fixed const & i );
+
+
+#endif
